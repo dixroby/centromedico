ddc7119a7d366a746b380362ca882fed24415cfe
dixroby <alestilodefe@gmail.com>
2020-05-21 16:04:59 -0500

    Feat: CRUD para pacientes

--- vista/index.php
+++ vista/index.php
@@ -282,6 +282,12 @@ if(!isset($_SESSION['S_IDUSUARIO'])){
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
+          <a onclick="cargar_contenido('contenido_principal','pacientes/vista_pacientes_listar.php')">
+            <i class="fa fa-medkit"></i> <span>Pacientes</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
         </li>
       </ul>
     </section>
