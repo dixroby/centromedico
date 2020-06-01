75f404fb53b7b019cff784e32a36f2982506f699
dixroby <alestilodefe@gmail.com>
2020-06-01 10:35:29 -0500

    Skype: Intento de session por persona

--- controlador/medico/controlador_crear_session.php
+++ controlador/medico/controlador_crear_session.php
@@ -1,20 +1,20 @@
 
 <?php
-$idmedico = $_POST['medico_id'];
-$medico_nombre = $_POST['medico_nombre'];
-$medico_apepat = $_POST['medico_apepat'];
-$medico_apemat = $_POST['medico_apemat'];
-$espeid = $_POST['especialidad_id'];
-$especialidad = $_POST['especialidad_nombre'];
+$idmedico = $_POST['id'];
+//$medico_nombre = $_POST['medico_nombre'];
+//$medico_apepat = $_POST['medico_apepat'];
+//$medico_apemat = $_POST['medico_apemat'];
+//$espeid = $_POST['especialidad_id'];
+//$especialidad = $_POST['especialidad_nombre'];
 
 session_start();
 
-$_SESSION['MEDICO_ID']=$idmedico;
-$_SESSION['MEDICO_NOMBRE']=$medico_nombre;
-$_SESSION['MEDICO_APEPAT']=$medico_apepat;
-$_SESSION['MEDICO_APEMAT']=$medico_apemat;
-$_SESSION['ESPECIALIDAD_ID']=$espeid;
-$_SESSION['ESPECIALIDAD_NOMBRE']=$especialidad;
+//$_SESSION['MEDICO_ID']=$idmedico;
+//$_SESSION['MEDICO_NOMBRE']=$medico_nombre;
+//$_SESSION['MEDICO_APEPAT']=$medico_apepat;
+//$_SESSION['MEDICO_APEMAT']=$medico_apemat;
+//$_SESSION['ESPECIALIDAD_ID']=$espeid;
+//$_SESSION['ESPECIALIDAD_NOMBRE']=$especialidad;
 
 header('Location: ../../vista/pas/especialidad.php');
 ?>
